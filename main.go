9665ad3d35905f4b123eb3c5b0f16874a94e749e
Akos <akos@Akoss-MacBook-Air.local>
2024-10-27 20:41:52 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+// SetupKubeClient initializes the Kubernetes clientset.
+func SetupKubeClient() (*kubernetes.Clientset, error) {
+	// Load kubeconfig from home directory
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+}
